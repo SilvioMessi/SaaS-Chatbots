5a323698a34f0252bfe3ca71f5e0c17496b156bf
Silvio Messi <messisilvio@gmail.com>
2017-03-30 13:09:37 +0200

    Extended the datasets.

--- js/prebuilt_entities.js
+++ js/prebuilt_entities.js
@@ -3,6 +3,16 @@ var prebuiltEntities = {
 		WIT : 'wit/number',
 		LUIS : 'number',
 		API : 'sys.number'
+	},
+	datetime:{
+		WIT : 'wit/datetime',
+		LUIS : 'datetime',
+		API : 'sys.date-time'
+	},
+	location:{
+		WIT : 'wit/location',
+		LUIS : 'geography',
+		API : 'sys.location'
 	}
 };
 module.exports = prebuiltEntities;
