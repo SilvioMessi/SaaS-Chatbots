5cf129f54bc52adb462dd3fe5677ce6ebbf43721
SilvioMessi <messisilvio@gmail.com>
2017-08-12 12:48:27 +0200

    Added function for compute the metrics (confusion matrix).

--- js/test.js
+++ js/test.js
@@ -35,31 +35,36 @@ function scoreLogic(service, queryProvided, resolvedQuery, intentsToFind,
 }
 
 function getMsChatbotScore(query, intentsToFind, entitiesToFind) {
-	return new Promise(function(resolve, reject) {
-		msChatbot.messageMeaning(query).then(
-				function(msChatbotResponse) {
-					var resolvedQuery = msChatbotResponse.query;
-					var intentsFound = [];
-					var entitiesFound = [];
-					var entities = msChatbotResponse.sentences[0].entities_recognized;
-					for (var index = 0; index < entities.length; ++index) {
-						var entity = {};
-						entity[entities[index].value.entity.name] = entities[index].value.value;
-						entitiesFound.push(entity);
-					}
-					var intents = msChatbotResponse.sentences[0].intents_recognized;
-					for (index = 0; index < intents.length; ++index) {
-						if (intents[index].score > 0.4) {
-							intentsFound.push(intents[index].intent);
-						}
-					}
-					resolve(scoreLogic('MS CHATBOT', query, resolvedQuery,
-							intentsToFind, intentsFound, entitiesToFind,
-							entitiesFound));
-				}, function(error) {
-					reject(error);
-				});
-	});
+	return new Promise(
+			function(resolve, reject) {
+				msChatbot
+						.messageMeaning(query)
+						.then(
+								function(msChatbotResponse) {
+									var resolvedQuery = msChatbotResponse.query;
+									var intentsFound = [];
+									var entitiesFound = [];
+									var entities = msChatbotResponse.sentences[0].entities_recognized;
+									for (var index = 0; index < entities.length; ++index) {
+										var entity = {};
+										entity[entities[index].value.entity.name] = entities[index].value.value;
+										entitiesFound.push(entity);
+									}
+									var intents = msChatbotResponse.sentences[0].intents_recognized;
+									for (index = 0; index < intents.length; ++index) {
+										if (intents[index].score > 0.4) {
+											intentsFound
+													.push(intents[index].intent);
+										}
+									}
+									resolve(scoreLogic('MS CHATBOT', query,
+											resolvedQuery, intentsToFind,
+											intentsFound, entitiesToFind,
+											entitiesFound));
+								}, function(error) {
+									reject(error);
+								});
+			});
 }
 
 function getApiScore(query, intentsToFind, entitiesToFind) {
@@ -178,11 +183,161 @@ function saveResults(results) {
 	});
 }
 
+function computeMetrics(results, serviceName) {
+	var supportMatrix = {};
+	var allClasses = [];
+	var confusionMatrix = {};
+	
+	// crate support matrix
+	for (var index = 0; index < results.length; ++index) {
+		var services = results[index];
+		for (var indexTwo = 0; indexTwo < services.length; ++indexTwo) {
+			var service = services[indexTwo];
+			if (service.service !== serviceName){
+				continue;
+			}
+			var intentsToFind = service.intentsToFind;
+			var _class = 'None';
+			if (intentsToFind.length > 0) {
+				_class = intentsToFind[0]
+			}
+
+			var intentsFound = service.intentsFound;
+			var predicted = 'None';
+			if (intentsFound.length > 0) {
+				predicted = intentsFound[0]
+			}
+
+			if (!(_class in supportMatrix)) {
+				supportMatrix[_class] = {}
+				supportMatrix[_class][predicted] = 0;
+				if (!(allClasses.includes(_class))) {
+					allClasses.push(_class);
+				}
+				if (!(allClasses.includes(predicted))) {
+					allClasses.push(predicted);
+				}
+
+			} else if (!(predicted in supportMatrix[_class])) {
+				supportMatrix[_class][predicted] = 0;
+				if (!(allClasses.includes(predicted))) {
+					allClasses.push(predicted);
+				}
+			}
+			supportMatrix[_class][predicted] += 1;
+		}
+	}
+
+	// fill support matrix with 0
+	for (var classId = 0; classId < allClasses.length; ++classId) {
+		_class = allClasses[classId];
+		for (var predictedId = 0; predictedId < allClasses.length; ++predictedId) {
+			predicted = allClasses[predictedId];
+			if (!(_class in supportMatrix)) {
+				supportMatrix[_class] = {}
+			}
+			if (!(predicted in supportMatrix[_class])) {
+				supportMatrix[_class][predicted] = 0;
+			}
+		}
+	}
+	
+	// save support matrix
+	var streamSupportMatrix = fs.createWriteStream('../support_matrix_' + serviceName + '.csv');
+	streamSupportMatrix.once('open', function(fd) {
+		streamSupportMatrix.write('class\\predicted;' + allClasses.join(';') + ';\n')
+		for (var classId = 0; classId < allClasses.length; ++classId) {
+			_class = allClasses[classId];
+			var row = [ _class + ';' ];
+			for (var predictedId = 0; predictedId < allClasses.length; ++predictedId) {
+				predicted = allClasses[predictedId];
+				row = row + supportMatrix[_class][predicted] + ';';
+			}
+			streamSupportMatrix.write(row + '\n');
+		}
+		streamSupportMatrix.end();
+	});
+	
+	var metrics = {}
+	var totTp = 0;
+	var totFn = 0;
+	var totFp = 0;
+	var totTn = 0;
+	// for each class
+	for (var classId = 0; classId < allClasses.length; ++classId) {
+		evaluatedClass = allClasses[classId];
+		metrics[evaluatedClass] = {};
+		// true positive
+		var tp = supportMatrix[evaluatedClass][evaluatedClass];
+		// false negative
+		var fn = 0;
+		// false positive
+		var fp = 0;
+		for (var predictedId = 0; predictedId < allClasses.length; ++predictedId) {
+			predicted = allClasses[predictedId];
+			if (predicted !== evaluatedClass) {
+				fn += supportMatrix[evaluatedClass][predicted];
+				fp += supportMatrix[predicted][evaluatedClass];
+			}
+		}
+		// true negative
+		var tn = 0;
+		for (var indexOne = 0; indexOne < allClasses.length; ++indexOne) {
+			_class = allClasses[indexOne];
+			for (var indexTwo = 0; indexTwo < allClasses.length; ++indexTwo) {
+				predicted = allClasses[indexTwo];
+				if (_class !== evaluatedClass && predicted !== evaluatedClass) {
+					tn += supportMatrix[_class][predicted];
+				}
+			}
+		}
+		metrics[evaluatedClass]['tp'] = tp;
+		metrics[evaluatedClass]['fn'] = fn;
+		metrics[evaluatedClass]['fp'] = fp;
+		metrics[evaluatedClass]['tn'] = tn;
+		totTp += tp;
+		totFn += fn;
+		totFp += fp;
+		totTn += tn;
+		
+	}
+	
+	totTp = totTp/allClasses.length;
+	totFn = totFn/allClasses.length;
+	totFp = totFp/allClasses.length;
+	totTn = totTn/allClasses.length;
+	metrics['totTp'] = totTp;
+	metrics['totFn'] = totFn;
+	metrics['totFp'] = totFp;
+	metrics['totTn'] = totTn;
+	
+	// save metrics
+	var streamMetrics = fs.createWriteStream('../metrics_' + serviceName + '.csv');
+	streamMetrics.once('open', function(fd) {
+		streamMetrics.write('class;tp;fn;fp;tn\n');
+		for (var classId = 0; classId < allClasses.length; ++classId) {
+			_class = allClasses[classId];
+			var row = util.format('%s;%s;%s;%s;%s\n', _class,
+					metrics[_class]['tp'], metrics[_class]['fn'],
+					metrics[_class]['fp'], metrics[_class]['tn']);
+			streamMetrics.write(row);
+		}
+		streamMetrics.write('\n');
+		var row = util.format('%s;%s;%s;%s;%s\n', 'avg',
+				metrics['totTp'], metrics['totFn'],
+				metrics['totFp'], metrics['totTn']);
+		streamMetrics.write(row);
+		
+		streamMetrics.end();
+	});
+}
+
 function test() {
 	return new Promise(function(resolve, reject) {
 		Promise.map(
 				queries,
 				function(query) {
+					console.log(query)
 					var queriesPromises = [];
 					queriesPromises.push(getMsChatbotScore(query.query,
 							query.intents, query.entities));
@@ -197,6 +352,10 @@ function test() {
 					concurrency : 1
 				}).then(function(results) {
 			saveResults(results);
+			computeMetrics(results, 'MS CHATBOT');
+			computeMetrics(results, 'API');
+			computeMetrics(results, 'WIT');
+			computeMetrics(results, 'LUIS');
 		}, function(error) {
 			reject(error);
 		});
